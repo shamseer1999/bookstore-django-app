82c80450ce0a4d652a95a34c6b042f58ae6528ac
Shamseer-tt <shamseer.primalcodes@gmail.com>
2023-08-09 18:05:10 +0530

    books and admins list pages

--- bookstoreAdmin/urls.py
+++ bookstoreAdmin/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.HomeListView.as_view(),name='mngr_home')
+    path('',views.HomeListView.as_view(),name='mngr_home'),
+    path('admins',views.AdminsListView.as_view(),name='admins_list'),
+    path('books',views.BooksListView.as_view(),name='books_list'),
 ]
--- bookstoreAdmin/views.py
+++ bookstoreAdmin/views.py
@@ -2,6 +2,7 @@ from django.views import View
 from django.shortcuts import render
 from .models import *
 from accounts.models import Admin
+from django.views.generic import ListView
 # Create your views here.
 
 class HomeListView(View):
@@ -15,5 +16,19 @@ class HomeListView(View):
         }
         
         return render(request,'mngr/index.html',context)
+    
+class AdminsListView(ListView):
+    
+    model = Admin
+    template_name = 'mngr/admin/index.html'
+    context_object_name = 'admins'
+    paginate_by = 10
+    
+class BooksListView(ListView):
+    
+    model = Book
+    template_name = 'mngr/books/index.html'
+    context_object_name = 'books'
+    paginate_by = 5
         
         
